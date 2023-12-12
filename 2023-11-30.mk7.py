132be5fb37edbe1a1ed885520430e2eae1a85842
Anupam Vipul <anupam.vipul+github@gmail.com>
2023-12-12 15:44:17 +0530

    ChatGPT4 Base
    Old code was feed and polished and a base for new start

--- 2023-11-30.mk7.py
+++ 2023-11-30.mk7.py
@@ -19,18 +19,21 @@ class SerialMonitor:
         self.init_ui()
 
     def init_ui(self):
+        # Baud Rate Configuration
         baud_rate_label = tk.Label(self.root, text="Baud Rate:")
         baud_rate_label.pack(pady=5)
         baud_rate_menu = tk.OptionMenu(self.root, self.baud_rate_var, "9600", "115200")
         baud_rate_menu.pack(pady=5)
         self.baud_rate_var.set("9600")  # Default baud rate
 
+        # COM Port Configuration
         com_port_label = tk.Label(self.root, text="COM Port:")
         com_port_label.pack(pady=5)
         com_port_menu = tk.OptionMenu(self.root, self.com_port_var, *[f"COM{i}" for i in range(10)])
         com_port_menu.pack(pady=5)
         self.com_port_var.set("COM7")  # Default COM port
 
+        # Monitoring Control Buttons
         self.start_button = tk.Button(self.root, text="Start Monitoring", command=self.start_monitoring)
         self.start_button.pack(pady=10)
 
@@ -40,10 +43,12 @@ class SerialMonitor:
         self.save_button = tk.Button(self.root, text="Save Data", command=self.save_data, state=tk.DISABLED)
         self.save_button.pack(pady=10)
 
+        # Status Display Label
         self.status_label = tk.Label(self.root, text="")
         self.status_label.pack(pady=10)
 
     def start_monitoring(self):
+        # Start serial data monitoring
         try:
             self.file_name = self.get_file_name()
             self.serial_port = serial.Serial(self.com_port_var.get(), int(self.baud_rate_var.get()))
@@ -56,6 +61,7 @@ class SerialMonitor:
             self.status_label["text"] = f"Error: {e}"
 
     def stop_monitoring(self):
+        # Stop serial data monitoring
         if self.serial_port:
             self.serial_port.close()
             self.start_button["state"] = tk.NORMAL
@@ -64,6 +70,7 @@ class SerialMonitor:
             self.status_label["text"] = "Monitoring stopped"
 
     def read_serial_data(self):
+        # Read and process serial data
         if self.serial_port and self.serial_port.is_open:
             try:
                 data = self.serial_port.readline().decode("utf-8").strip()
@@ -81,25 +88,26 @@ class SerialMonitor:
                 self.root.after(100, self.read_serial_data)
 
     def get_file_name(self):
+        # Get file name for saving data
         date_prefix = datetime.now().strftime("%Y-%m-%d %H.%M.%S.%f")[:-3]
         file_suffix = tkinter.simpledialog.askstring("File Suffix", "Enter file suffix (or leave empty):")
         file_suffix = f"{file_suffix}" if file_suffix else ""
-        #file_name = f"{date_prefix}-{file_suffix.replace('.', '')}.csv"
-        file_name = f"{date_prefix} {file_suffix}.csv" #  pampam mod 
+        file_name = f"{date_prefix} {file_suffix}.csv"
         return filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")], initialfile=file_name)
 
-
-
-    def save_data(self):
-        pass  # Not used in this version of the script
-
     def save_data_to_file(self, data):
+        # Save data to file
         if self.file_name:
             with open(self.file_name, "a", newline="") as file:
                 file.write(data)
             self.status_label["text"] = f"Data saved to {self.file_name}"
 
+    def save_data(self):
+        # Placeholder for saving data functionality
+        pass
+
+# Tkinter GUI initialization
 if __name__ == "__main__":
     root = tk.Tk()
-    monitor = SerialMonitor(root)
+    app = SerialMonitor(root)
     root.mainloop()
